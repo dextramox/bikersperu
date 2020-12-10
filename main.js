9cd76bf9d6358f8defde7d2557e54a4814533ec9
dextramox <30883061+dextramox@users.noreply.github.com>
2020-12-09 22:33:55 -0500

    navidad
    nav 1

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+document.querySelector('.nav-menu').classList.toggle('show');     
+}
+    );
+    ScrollReveal().reveal('.showcase');
+    ScrollReveal().reveal('.news.car',{ delay:500});
+    ScrollReveal().reveal('.cards-banner',{ delay:500});
+
+
