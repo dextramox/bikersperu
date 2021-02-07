823597da8466e0198100d496fa117df9f31eee47
dextramox <30883061+dextramox@users.noreply.github.com>
2021-02-07 12:41:07 -0500

    newone
    renovar todo nuevo

--- main.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menu-btn').addEventListener('click',()=>{
-document.querySelector('.nav-menu').classList.toggle('show');     
-}
-    );
-    ScrollReveal().reveal('.showcase');
-    ScrollReveal().reveal('.news.car',{ delay:500});
-    ScrollReveal().reveal('.cards-banner',{ delay:500});
-
-
